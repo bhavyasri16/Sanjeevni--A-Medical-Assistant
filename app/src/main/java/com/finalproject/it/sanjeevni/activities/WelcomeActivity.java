25f620d3d812b5c646e66311ff3c3fa5aa1f24fe
unknown <shivig1798@gmail.com>
2020-08-15 12:43:07 +0530

    chatbot update

--- app/src/main/java/com/finalproject/it/sanjeevni/activities/WelcomeActivity.java
+++ app/src/main/java/com/finalproject/it/sanjeevni/activities/WelcomeActivity.java
@@ -349,7 +349,7 @@ public class WelcomeActivity extends AppCompatActivity {
                         } else if (position == 1) {
                             startActivity(new Intent(WelcomeActivity.this, BloodBank.class));
                         } else if (position == 2) {
-                            //startActivity(new Intent(WelcomeActivity.this, com.finalproject.it.sanjeevni.activities.chatBot.chatBot.class));
+                            //startActivity(new Intent(WelcomeActivity.this, com.finalproject.it.sanjeevni.activities.Reminder.DashBoardActivity.class));
                         } else {
                             startActivity(new Intent(WelcomeActivity.this, com.finalproject.it.sanjeevni.activities.chatBot.chatBot.class));
 
