e107eb2d9e8557f14172e3ffdc0652c18d09c2cd
unknown <shivig1798@gmail.com>
2020-09-08 22:01:15 +0530

    reminder updated

---
+++ app/src/main/java/com/finalproject/it/sanjeevni/activities/AlarmReceiver1.java
@@ -0,0 +1,51 @@
+package com.finalproject.it.sanjeevni.activities;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+import androidx.core.app.NotificationCompat;
+
+import com.finalproject.it.sanjeevni.R;
+
+import java.util.EventListener;
+
+
+    public class AlarmReceiver1 extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+
+            long when = System.currentTimeMillis();
+            NotificationManager notificationManager = (NotificationManager) context
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+
+            Intent notificationIntent = new Intent(context, EventListener.class);
+            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                    notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+            Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+            NotificationCompat.Builder mNotifyBuilder = new NotificationCompat.Builder(
+                    context)
+                    .setContentTitle("Alarm Fired")
+                    .setContentText("Events to be Performed").setSound(alarmSound)
+                    .setAutoCancel(true).setWhen(when)
+                    .setContentIntent(pendingIntent)
+                    .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
+            int MID=0;
+            notificationManager.notify(MID, mNotifyBuilder.build());
+            MID++;
+
+        }
+
+    }
+
